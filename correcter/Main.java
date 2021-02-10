f6c94d8ecabbfaf875271818265b264d020ffb65
Mr.Shtein <mr.shtein61@yandex.ru>
2021-02-10 20:16:21 +0300

    Complete the first stage

--- correcter/Main.java
+++ correcter/Main.java
@@ -1,7 +1,16 @@
 package correcter;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        Scanner scan = new Scanner(System.in);
+        String phraseToMix = scan.nextLine();
+        SymbolsMixer symbolsMixer = new SymbolsMixer(phraseToMix, ' ', '~');
+        String mixedPhrase = symbolsMixer.mixedPhrase();
+        System.out.println(mixedPhrase);
+
+
     }
 }
