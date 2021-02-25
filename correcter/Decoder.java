da1cc84010b9f3bcd98f607273d34ee1a721d743
Mr.Shtein <mr.shtein61@yandex.ru>
2021-02-25 17:09:46 +0300

    getNumberOfIncorrectPair completed

--- correcter/Decoder.java
+++ correcter/Decoder.java
@@ -7,10 +7,36 @@ public class Decoder {
             byte byteToDecode = bytesToDecode[i];
 
         }
+        return null;
     }
 
-    public byte decodeByte(byte byteToDecode) {
-
+    public void searchAndDecodeByte(byte byteToDecode) {
+        int incorrectPairNum = 0;
+        try {
+            incorrectPairNum = getNumberOfIncorrectPair(byteToDecode);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        System.out.println(incorrectPairNum);
     }
+
+    public int getNumberOfIncorrectPair(byte researchingByte) throws Exception {
+        int numOfPair = 4;
+        for (int i = 0; i < 8; i += 2) {
+            int firstMask = 1 << i;
+            int secMask = 1 << (i + 1);
+
+            int firstBit = researchingByte & firstMask;
+            int secBit = (researchingByte & secMask) >> 1;
+
+            if (firstBit != secBit) {
+                return numOfPair;
+            }
+            numOfPair--;
+        }
+        throw new Exception("Correct byte");
+    }
+
+
 
 }
